4b26863664b4d688cc12732a697398d593bccfd4
Andy Gelfo <andyjgelfo@gmail.com>
2024-06-06 15:40:39 -0400

    completed second homepage section, "versatile developer"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,13 +17,54 @@ export class HomeComponent {
 
     const writer = new Typewriter(target, {
       loop: true,
-      typeColor: 'blue'
+      typeColor: 'white',
+      typeSpeed: 80,
+      deleteSpeed: 80
     })
     
     writer
-      .type('A simple syntax makes it easy.')
-      .rest(500)
-      .start()
+    .type('-> UCF Alumni: Computer Science Major')
+    .rest(500)
+    .remove(22)
+    .type('Digital Media Minor')
+    .rest(500)
+    .remove(31)
+    .type('Software & Web Developer')
+    .rest(500)
+    .remove(24)
+    // .changeOps({ deleteSpeed: 20 })
+    // .remove(21)
+    .type('Summa Cum Laude Graduate')
+    .rest(500)
+    .clear()
+    .start()
+
+    const target2 = document.querySelector('.tw2');
+
+    const writer2 = new Typewriter(target2, {
+      loop: true,
+      typeColor: 'white',
+      typeSpeed: 80,
+      deleteSpeed: 80
+    })
+    
+    writer2
+    .type('-> UCF Alumni: Computer Science Major')
+    .rest(500)
+    .remove(22)
+    .type('Digital Media Minor')
+    .rest(500)
+    .remove(31)
+    .type('Software & Web Developer')
+    .rest(500)
+    .remove(24)
+    // .changeOps({ deleteSpeed: 20 })
+    // .remove(21)
+    .type('Summa Cum Laude Graduate')
+    .rest(500)
+    .clear()
+    .start()
+      
               
   }
 

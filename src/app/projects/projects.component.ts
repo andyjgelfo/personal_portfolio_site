72e44df47c41bdf8141a73ed1f981568e035211f
Andy Gelfo <andyjgelfo@gmail.com>
2024-06-12 19:13:47 -0400

    added cerealboxd to projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -28,7 +28,17 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
           ]
         )
       ]
-    )
+    ),
+
+    trigger(
+        'rotateAnimation', 
+        [
+            state('default', style({ transform: 'rotate(0deg)' })),
+            state('rotated', style({ transform: 'rotate(-180deg)' })),
+            transition('rotated => default', animate('0.3s ease')),
+            transition('default => rotated', animate('0.3s ease'))
+        ]
+      )
   ]
 
 })
@@ -38,12 +48,55 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 export class ProjectsComponent {
 
     kgIsCollapsed1 = false;
-    kgIsCollapsed2 = false;
-    kgIsCollapsed3 = false;
+    kgIsCollapsed2 = true;
+    kgIsCollapsed3 = true;
 
     kgShowVideo = true;
     kgShowCarousel = false;
     active = 1;
+
+    state: string = 'default';
+
+    kgRotateState1: string = 'rotated';
+    kgRotateState2: string = 'default';
+    kgRotateState3: string = 'default';
+
+    cbShowVideoWeb = true;
+    cbShowVideoMobile = false;
+    cbShowCarousel =  false;
+
+
+    rotate() {
+        this.state = (this.state === 'default' ? 'rotated' : 'default');
+    }
+
+    kgRotate(num: number) {
+
+        console.log(num);
+
+        switch(num) {
+            case 1:
+                this.kgRotateState1 = (this.kgRotateState1 === 'default' ? 'rotated' : 'default');
+                break;
+            case 2:
+                this.kgRotateState2 = (this.kgRotateState2 === 'default' ? 'rotated' : 'default');
+                break;
+            case 3:
+                this.kgRotateState3 = (this.kgRotateState3 === 'default' ? 'rotated' : 'default');
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    kgRotate2() {
+        this.state = (this.state === 'default' ? 'rotated' : 'default');
+    }
+
+    kgRotate3() {
+        this.state = (this.state === 'default' ? 'rotated' : 'default');
+    }
 
 
 
